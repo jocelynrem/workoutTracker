5bc6594e1849910757ea6d56fd753188ea696175
jocelynrem <jocelynrem@gmail.com>
2021-08-01 10:46:53 -0400

    last wrkout duration works

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,30 @@
 const router = require("express").Router();
 const Workout = require("../models/Workout.js");
 
+const duration = { totalDuration: { $sum: "$exercises.duration" } };
+
 router.post("/api/workout", async (req, res) => {
   try {
-        var result = await Workout.save();
-        res.send(result);
-  } catch(err) {
+    var result = await Workout.save();
+    res.status(200).json(result);
+  } catch (err) {
     res.status(400).send(err)
   };
 });
 
-router.get("/api/workouts", async (req, res) => {
+router.get('/api/workouts', async (req, res) => {
   try {
-    var result = await Workout.find().exec();
-    res.send(result);
+    var result = await Workout.aggregate([{ $set: duration }]);
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(400).send(err)
+  }
+})
+
+router.get("/api/workouts/range", async (req, res) => {
+  try {
+    var result = await Workout.aggregate([{ $set: duration }, { $sort: { 'day': -1 } }, { $limit: 7 }]);
+    res.status(200).json(result);
   } catch (err) {
     res.status(400).send(err)
   }
