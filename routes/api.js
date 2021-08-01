d17d7313b94d3cabc4568f24f8ac4c1bf932fc66
jocelynrem <jocelynrem@gmail.com>
2021-08-01 11:28:45 -0400

    put and post written, not working

--- routes/api.js
+++ routes/api.js
@@ -3,30 +3,43 @@ const Workout = require("../models/Workout.js");
 
 const duration = { totalDuration: { $sum: "$exercises.duration" } };
 
-router.post("/api/workout", async (req, res) => {
+router.post("/api/workout", async ({ body }, res) => {
   try {
-    var result = await Workout.save();
+    const result = await Workout.create(body);
     res.status(200).json(result);
   } catch (err) {
-    res.status(400).send(err)
+    res.status(400).json(err)
   };
 });
 
+router.put("api/workouts/:id", async (req, res) => {
+  try {
+    const result = await Workout.aggregate([
+      { $match: { _id: req.params.id } },
+      { $addFields: { exercises: req.body } }
+    ])
+    res.status(200).json(result);
+    console.log('result:', result)
+  } catch (err) {
+    res.status(400).json(err)
+  }
+})
+
 router.get('/api/workouts', async (req, res) => {
   try {
-    var result = await Workout.aggregate([{ $set: duration }]);
+    const result = await Workout.aggregate([{ $set: duration }]);
     res.status(200).json(result);
   } catch (err) {
-    res.status(400).send(err)
+    res.status(400).json(err)
   }
 })
 
 router.get("/api/workouts/range", async (req, res) => {
   try {
-    var result = await Workout.aggregate([{ $set: duration }, { $sort: { 'day': -1 } }, { $limit: 7 }]);
+    const result = await Workout.aggregate([{ $set: duration }, { $sort: { 'day': -1 } }, { $limit: 7 }]);
     res.status(200).json(result);
   } catch (err) {
-    res.status(400).send(err)
+    res.status(400).json(err)
   }
 })
 
