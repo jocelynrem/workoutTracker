8a3b5c9b8bd0af986ca35735b10c3fcd9b5122e1
jocelynrem <jocelynrem@gmail.com>
2021-08-01 10:00:22 -0400

    updated routes

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,22 @@
 const router = require("express").Router();
 const Workout = require("../models/Workout.js");
 
-
-
-// router.post("/api/exercise", ({ body }, res) => {
-//     Workout.create(body)
-//       .then(dbWorkout => {
-//         res.json(dbWorkout);
-//       })
-//       .catch(err => {
-//         res.status(400).json(err);
-//       });
-//   });
-
-
-router.get("/exercise", (req, res) => {
-  Workout.find({})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
+router.post("/api/workout", async (req, res) => {
+  try {
+        var result = await Workout.save();
+        res.send(result);
+  } catch(err) {
+    res.status(400).send(err)
+  };
 });
+
+router.get("/api/workouts", async (req, res) => {
+  try {
+    var result = await Workout.find().exec();
+    res.send(result);
+  } catch (err) {
+    res.status(400).send(err)
+  }
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -17,6 +17,11 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 
 mongoose.Promise = global.Promise;
 
+const db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 app.use(require("./routes/html.js"));
 app.use(require("./routes/api.js"));
 
