c251d8f72ca0db416d0094ebe5f961abd6edbba8
jocelynrem <jocelynrem@gmail.com>
2021-08-03 18:30:29 -0400

    poast route updated

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { response } = require("express");
 const Workout = require("../models/Workout");
 
 const duration = { totalDuration: { $sum: "$exercises.duration" } };
@@ -21,28 +22,28 @@ router.get("/api/workouts/range", async (req, res) => {
   }
 })
 
-router.post("/api/workouts", async ({ body }, res) => {
-  try {
-    const result = new Workout.create(body);
-    await result.save()
-    res.status(200).json(result);
-  } catch (err) {
-    res.status(400).json(err)
-  };
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+    .then((response) => {
+      res.json(response);
+    })
+    .catch((err) => {
+      res.json(err);
+    })
 });
 
 router.put("/api/workouts/:id", async (req, res) => {
-  try {
-    const result =  Workout.aggregate([
+
+    Workout.aggregate([
       { $match: { _id: req.params.id } },
       { $addFields: { exercises: req.body } }
     ])
-    await result.save()
-    res.status(200).json(result);
-    console.log('result:', result)
-  } catch (err) {
-    res.status(400).json(err)
-  }
+    .then((response) => {
+      res.json(response)
+    })
+    .then((err) => {
+      res.json(err)
+    })
 })
 
 module.exports = router;
